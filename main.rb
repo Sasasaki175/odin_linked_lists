a4371671b71215e6ee174c7837174d18c8723a65
Sasasaki175 <207643966+Sasasaki175@users.noreply.github.com>
2025-08-27 11:01:41 +0900

    Create file to test lib/linked_lists.rb

---
+++ main.rb
@@ -0,0 +1,25 @@
+require_relative 'lib/linked_lists'
+
+list = LinkedList.new
+
+list.append('dog')
+list.append('cat')
+list.append('parrot')
+list.append('hamster')
+list.append('snake')
+list.append('turtle')
+
+puts list
+
+puts list.size
+puts list.at(5)
+puts list.at(0)
+puts list.at(6)
+puts list.find('turtle')
+p list.find('monkey')
+list.prepend('owl')
+list.pop
+
+puts list
+
+puts list.contains?('snake')
